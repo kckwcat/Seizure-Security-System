d2289eb75690227be76a666a5ce3cdf0594960d9
kckwcat <84925692+kckwcat@users.noreply.github.com>
2022-08-28 11:17:25 -0400

    Update Motion.py

--- Motion.py
+++ Motion.py
@@ -13,27 +13,27 @@ pixel_mean = 0
 # initialize pygame
 pygame.init()
 
-while(True):
-    # get current frame of camera
-    ret, frame = camera.read()
-    cv2.imshow('frame', frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-
 # path folder for loading files
 PROJECT_ROOT = Path(__file__).parent.parent
 
 # variable to see if motion is detected or not
 detected_motion = False
+
+while(True):
+    # get current frame of camera
+    ret, frame = camera.read()
+    cv2.imshow('frame', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     # compare mean values of all pixels of 2 consecutive frames, then subtract the 2 mean values
-    result = np.abs(np.mean(gray) - pixel_mean) # variable for change in frame
+    frame_diff = np.abs(np.mean(gray) - pixel_mean) # variable for change in frame
     # change in frame determines motion
     
     # update the pixel mean each time with each new frame
     pixel_mean = np.mean(gray)
     
     # if motion detected, detected_motion is True, and sound will play and let caregivers know that the child has woken up
-    if result > 2:
+    if frame_diff > 2:
         print("Motion detected!")
         detected_motion = True
         mixer.music.load(PROJECT_ROOT / "Ignition Hacks 2022/0125. Imagination - AShamaluevMusic.mp3")
