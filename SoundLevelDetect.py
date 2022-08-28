1a6986c7431d9fd525b57c8445c5a9ee730d4b23
kckwcat <84925692+kckwcat@users.noreply.github.com>
2022-08-28 11:46:43 -0400

    Update SoundLevelDetect.py

--- SoundLevelDetect.py
+++ SoundLevelDetect.py
@@ -7,7 +7,7 @@ pygame.init()
 # alarm sound module
 alarm_sound = pygame.mixer.Sound("mixkit-alarm-digital-clock-beep-989.wav")
 
-
+# alarm sound function
 def alert_sound():
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
@@ -22,6 +22,7 @@ def alert_sound():
         volume_detected = np.linalg.norm(audio_data)*10
         
         # if possible seizure detected, alarm will sound
+        # I don't know the exact number of decibels for a seizure sound, so I just shouted to decide the value (volume level) for volume_detected to compare to
         if volume_detected > 900000:
             print('oh no')
             alarm_sound.set_volume(0.8)
